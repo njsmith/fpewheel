0620d8e08e9f6ea6d2f60692176c63e09ade233e
Nathaniel J. Smith <njs@pobox.com>
2016-12-24 21:53:16 -0800

    Add a bit more metadata

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from setuptools import setup, Extension
 
 setup(name="fpewheel",
+      author="Nathaniel J. Smith",
+      author_email="njs@pobox.com",
+      description="Test case for PyFPE_* macros",
+      url="https://github.com/njsmith/fpewheel",
+      classifiers=[
+          "License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
+      ],
       ext_modules=[Extension("fpewheel", sources=["fpewheel.c"])])
